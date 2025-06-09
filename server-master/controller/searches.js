e518c6bb6d77929b7172eac6ed9a980708747bc5
AvigailRothman <avigailrothman@gmail.com>
2025-06-09 20:13:20 +0300

    0

--- server-master/controller/searches.js
+++ server-master/controller/searches.js
@@ -1,45 +1,45 @@
-export const getSearch = (req) => {
-  const { search, id, userId, completed, _sort, _order } = req.query;
+// export const getSearch = (req) => {
+//   const { search, id, userId, completed, _sort, _order } = req.query;
 
-  let conditions = [];
-  let params = [];
+//   let conditions = [];
+//   let params = [];
 
-  if (id) {
-    conditions.push('id = ?');
-    params.push(Number(id));
-  }
+//   if (id) {
+//     conditions.push('id = ?');
+//     params.push(Number(id));
+//   }
 
-  if (userId) {
-    conditions.push('user_id = ?');
-    params.push(Number(userId));
-  }
+//   if (userId) {
+//     conditions.push('user_id = ?');
+//     params.push(Number(userId));
+//   }
 
-  if (completed === 'true' || completed === 'false') {
-    conditions.push('completed = ?');
-    params.push(completed === 'true' ? 1 : 0);
-  }
+//   if (completed === 'true' || completed === 'false') {
+//     conditions.push('completed = ?');
+//     params.push(completed === 'true' ? 1 : 0);
+//   }
 
-  if (search) {
-    conditions.push('title LIKE ?');
-    params.push(`%${search}%`);
-  }
+//   if (search) {
+//     conditions.push('title LIKE ?');
+//     params.push(`%${search}%`);
+//   }
 
-  let sql = '';
+//   let sql = '';
 
-  if (conditions.length > 0) {
-    sql += ' WHERE ' + conditions.join(' AND ');
-  }
+//   if (conditions.length > 0) {
+//     sql += ' WHERE ' + conditions.join(' AND ');
+//   }
 
-  if (_sort && _order) {
-    const allowedSorts = ['id', 'title', 'userId', 'completed'];
-    const allowedOrders = ['asc', 'desc'];
+//   if (_sort && _order) {
+//     const allowedSorts = ['id', 'title', 'userId', 'completed'];
+//     const allowedOrders = ['asc', 'desc'];
 
-    if (allowedSorts.includes(_sort) && allowedOrders.includes(_order.toLowerCase())) {
-      sql += ` ORDER BY ${_sort} ${_order.toUpperCase()}`;
-    }
-  }
+//     if (allowedSorts.includes(_sort) && allowedOrders.includes(_order.toLowerCase())) {
+//       sql += ` ORDER BY ${_sort} ${_order.toUpperCase()}`;
+//     }
+//   }
 
-  return { sql, params };
+//   return { sql, params };
 
 
-}
+// }
