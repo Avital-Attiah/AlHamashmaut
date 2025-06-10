458c052bb37ce92cee34ac2820f1b007195282bd
Avital-Attiah <avitala5515@gmail.com>
2025-06-11 01:40:11 +0300

    שינוי בקליינט

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -60,6 +60,21 @@ return (
           <div key={n} className="gallery-item">תמונה</div>
         ))}
       </div>
+{/* רשתות חברתיות */}
+<div className="social-links">
+  <a href="https://x.com/al_hamashmaut" target="_blank" rel="noreferrer">
+    <img src="/icons/x.svg" alt="X" />
+  </a>
+  <a href="https://www.instagram.com/tamirdortal/" target="_blank" rel="noreferrer">
+    <img src="/icons/instagram.svg" alt="Instagram" />
+  </a>
+  <a href="https://www.youtube.com/@alhamashmaut" target="_blank" rel="noreferrer">
+    <img src="/icons/youtube.svg" alt="YouTube" />
+  </a>
+  <a href="#" title="ספוטיפיי (בקרוב)">
+    <img src="/icons/spotify.svg" alt="Spotify" />
+  </a>
+</div>
 
       
      
